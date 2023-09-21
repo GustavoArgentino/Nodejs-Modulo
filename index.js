9326cd4cbfedd002b4553520bc53f25c72918179
Gustavo Argentino <125594970+GustavoArgentino@users.noreply.github.com>
2023-09-21 12:51:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var http = require('http');
-var cadastro = require('./myRegister');
-var login = require('./login');
-var home = require ('./home');
-var consultas = require ('./consultas');
+const http = require('http');
+const cadastro = require('./myRegister');
+const login = require('./login');
+const home = require('./home');
+const consultas = require('./consultas');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write("Home: " + home.home() + "<br>");
-  res.write("Cadastre-se: " + cadastro.myRegister() + "<br>");
-  res.write("Login: " + login.login() + "<br>");
-  res.write("Consultas: " + consultas.consultas());
+http.createServer((req, res) => {
+  res.writeHead(200, { 
+    'Content-Type': 'text/html',
+    'font-family': 'Arial, sans-serif',
+    'background-color': '#f0f0f0',
+    'color': '#333',
+    'padding': '10px',
+  });
+
+  res.write(`<h1>Home:</h1>${home.home()}<br>`);
+  res.write(`<h1>Cadastre-se:</h1>${cadastro.myRegister()}<br>`);
+  res.write(`<h1>Login:</h1>${login.login()}<br>`);
+  res.write(`<h1>Consultas:</h1>${consultas.consultas()}`);
   res.end();
 }).listen(5015);
